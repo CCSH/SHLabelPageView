0e55a65839d42000b33b847002d189cf96cbcb90
CCSH <624089195@qq.com>
2019-09-03 08:48:54 +0800

    v 2.2.0 1、新增标签宽度不同选中线不同 2、新增改变文字宽高 3、优化选中线拉伸动画计算 4、选中线深度定制

---
+++ SHLabelPageViewExample/Pods/Target Support Files/Pods-SHLabelPageViewExample-SHLabelPageViewExampleUITests/Pods-SHLabelPageViewExample-SHLabelPageViewExampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SHLabelPageViewExample_SHLabelPageViewExampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SHLabelPageViewExample_SHLabelPageViewExampleUITests
+@end
--- SHLabelPageViewExample/SHLabelPageViewExample/ViewController.m
+++ SHLabelPageViewExample/SHLabelPageViewExample/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "SHLabelPageView.h"
 
-@interface ViewController ()<UIScrollViewDelegate>
+@interface ViewController () <UIScrollViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIScrollView *bigScroll;
 
@@ -22,78 +22,91 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
+
     self.bigScroll.delegate = self;
-    
+
     //初始化
-    self.pageView = [[SHLabelPageView alloc]init];
+    self.pageView = [[SHLabelPageView alloc] init];
     self.pageView.frame = CGRectMake(0, 64, self.view.frame.size.width, 50);
     //回调
     self.pageView.pageViewBlock = ^(SHLabelPageView *pageView) {
-        
+
         //最好不要加动画
-        [self.bigScroll setContentOffset:CGPointMake(pageView.index *self.view.frame.size.width, 0) animated:NO];
+        [self.bigScroll setContentOffset:CGPointMake(pageView.index * self.view.frame.size.width, 0) animated:NO];
     };
-    self.pageView.currentLine.y = 40;
-    self.pageView.line.backgroundColor = [UIColor orangeColor];
-    
+
     [self btnAction:nil];
-    
+
     [self.view addSubview:self.pageView];
-    
+
     [self addScrollPage];
 }
 
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
-    
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
     if ([scrollView isEqual:self.bigScroll]) {
-
         CGFloat value = scrollView.contentOffset.x / scrollView.frame.size.width;
-        
+
         //设置偏移
         self.pageView.contentOffsetX = value;
     }
 }
-- (IBAction)btnAction:(id)sender {
-    
 
+- (IBAction)btnAction:(id)sender {
     if (self.pageView.type == SHLabelPageType_one) {
-        
         self.pageView.startX = 20;
         self.pageView.spaceW = 20;
         self.pageView.type = SHLabelPageType_more;
-        self.pageView.pageList = @[@"头条",@"娱乐",@"热点",@"体育",@"泉州",@"网易号",@"财经",@"科技",@"汽车",@"时尚",@"图片",@"跟贴",@"房产",@"直播",@"轻松一刻",@"段子",@"军事",@"历史",@"家居",@"独家",@"游戏",@"健康",@"政务",@"哒哒趣闻",@"美女",@"NBA",@"社会",@"彩票",@"漫画",@"影视歌",@"中国足球",@"国际足球",@"CBA",@"跑步",@"手机",@"数码",@"移动互联",@"云课堂",@"态度公开课",@"旅游",@"读书",@"酒香",@"教育",@"亲子",@"暴雪游戏",@"情感",@"艺术",@"博客",@"论坛",@"型男",@"萌宠"];
+        self.pageView.pageList = @[ @"头条", @"娱乐", @"热点", @"体育", @"泉州", @"网易号", @"财经", @"科技", @"汽车", @"时尚", @"图片", @"跟贴", @"房产", @"直播", @"轻松一刻", @"段子", @"军事", @"历史", @"家居", @"独家", @"游戏", @"健康", @"政务", @"哒哒趣闻", @"美女", @"NBA", @"社会", @"彩票", @"漫画", @"影视歌", @"中国足球", @"国际足球", @"CBA", @"跑步", @"手机", @"数码", @"移动互联", @"云课堂", @"态度公开课", @"旅游", @"读书", @"酒香", @"教育", @"亲子", @"暴雪游戏", @"情感", @"艺术", @"博客", @"论坛", @"型男", @"萌宠" ];
         self.pageView.labelTag = nil;
         self.pageView.index = 0;
-    }else{
 
-//        self.pageView.startX = 0;
+        //选中线
+//        self.pageView.currentLineY = 10;
+//        self.pageView.currentLineSize = CGSizeMake(0, 30);
+//        self.pageView.currentLineMargin = 20;
+//        self.pageView.currentLineColor = [UIColor orangeColor];
+//        self.pageView.currentLineRadius = 15;
+//        self.pageView.checkColor = [UIColor whiteColor];
+        
+        self.pageView.currentLineY = 40;
+        self.pageView.currentLineSize = CGSizeMake(0, 4);
+        self.pageView.currentLineMargin = 0;
+        self.pageView.currentLineColor = [UIColor orangeColor];
+        self.pageView.currentLineRadius = 2;
+        self.pageView.checkColor = [UIColor blackColor];
+
+    } else {
         self.pageView.spaceW = 30;
         self.pageView.type = SHLabelPageType_one;
-        self.pageView.pageList = @[@"关注",@"热门",@"最新"];
-        self.pageView.labelTag = @{@"2":[NSValue valueWithCGRect:CGRectMake(32, 15, 8, 8)],
-                                   @"1":[NSValue valueWithCGRect:CGRectMake(32, 15, 8, 8)]};
+        self.pageView.pageList = @[ @"关注", @"热门", @"最新" ];
+        self.pageView.labelTag = @{ @"2" : [NSValue valueWithCGRect:CGRectMake(32, 15, 8, 8)] };
         self.pageView.index = 1;
+
+        //选中线
+        self.pageView.currentLineY = 40;
+        self.pageView.currentLineSize = CGSizeMake(20, 4);
+        self.pageView.currentLineColor = [UIColor redColor];
+        self.pageView.currentLineRadius = 2;
+
+        self.pageView.checkColor = [UIColor blackColor];
     }
 
     [self.pageView reloadView];
-    
+
     [self addScrollPage];
 }
 
-- (void)addScrollPage{
-    
+- (void)addScrollPage {
     for (UIView *view in self.bigScroll.subviews) {
         [view removeFromSuperview];
     }
-    
+
     for (int i = 0; i < self.pageView.pageList.count; i++) {
-        
-        UILabel *lab = [[UILabel alloc]init];
-        lab.frame = CGRectMake(i*self.view.frame.size.width,30, self.view.frame.size.width, 30);
+        UILabel *lab = [[UILabel alloc] init];
+        lab.frame = CGRectMake(i * self.view.frame.size.width, 30, self.view.frame.size.width, 30);
         lab.backgroundColor = [UIColor redColor];
         lab.textAlignment = NSTextAlignmentCenter;
-        lab.text = [NSString stringWithFormat:@"当前是第  %d   个",i];
+        lab.text = [NSString stringWithFormat:@"当前是第  %d   个", i];
         [self.bigScroll addSubview:lab];
     }
     self.bigScroll.contentSize = CGSizeMake(self.pageView.pageList.count * self.view.frame.size.width, self.bigScroll.frame.size.height);
